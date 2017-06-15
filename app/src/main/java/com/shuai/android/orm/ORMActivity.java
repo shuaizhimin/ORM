abca821d1440dacf7aec675f568b03dbbb0b3a29
帅治敏 <shuaizhimin0717@163.com>
2017-06-15 10:39:24 +0800

    初始化仓库

---
+++ app/src/main/java/com/shuai/android/orm/ORMActivity.java
@@ -0,0 +1,13 @@
+package com.shuai.android.orm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ORMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orm);
+    }
+}
